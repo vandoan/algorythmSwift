e59cd74af82831a341f157b57bbf6e705fbece31
Van Doan <vdoan09@gmail.com>
2016-03-28 23:19:16 -0400

    added data and images and connected new controllers

---
+++ PlaylistMasterViewController.swift
@@ -0,0 +1,40 @@
+ //
+//  ViewController.swift
+//  Algorhythm
+//
+//  Created by Van Doan  on 3/27/16.
+//  Copyright © 2016 com.vandoona. All rights reserved.
+//
+
+import UIKit
+
+class PlaylistMasterViewController: UIViewController {
+
+    @IBOutlet weak var abutton: UIButton!
+    @IBOutlet weak var playlistImageView0: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        abutton.setTitle("Press me", forState: .Normal)
+        
+        let playlist = Playlist(index: 0)
+        playlistImageView0.image = playlist.icon
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?){
+        if segue.identifier == "showPlaylistDetail" {
+            let playlistDetailController = segue.destinationViewController as! PlaylistDetailViewController
+            playlistDetailController.segueLabelText = "Yay! You pressed the button!"
+            
+        }
+    }
+    
+}
+
